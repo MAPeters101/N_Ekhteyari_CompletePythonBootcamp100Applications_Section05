5ff6183dd4005b91035231bec229ea069aca265f
Mark Peters <mapeters101@earthlink.net>
2024-11-25 10:42:48 -0500

    Lesson042:  Work on lesson.

---
+++ modules/lesson042.py
@@ -0,0 +1,7 @@
+import math_operations
+
+result_add = math_operations.add(4,5)
+result_subtract = math_operations.subtract(12,6)
+
+print(f"Addition: {result_add}")
+print(f"Subtracttion: {result_subtract}")
